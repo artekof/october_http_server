14b2e9dc126b91a972f1ad93b52efcdb3a531e7f
Artem <artekof.one@gmail.com>
2024-11-04 00:58:27 +0300

    homework1

--- src/main/java/ru/otus/october/http/server/HttpServer.java
+++ src/main/java/ru/otus/october/http/server/HttpServer.java
@@ -7,25 +7,37 @@ import java.net.Socket;
 public class HttpServer {
     private int port;
     private Dispatcher dispatcher;
+    private boolean isActive;
 
     public HttpServer(int port) {
         this.port = port;
         this.dispatcher = new Dispatcher();
+        this.isActive = true;
     }
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущен на порту: " + port);
-            try (Socket socket = serverSocket.accept()) {
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
+            while (isActive) {
+                Socket socket = serverSocket.accept();
+                new Thread(() -> dispatcherHandler(socket)).start();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    private void dispatcherHandler(Socket socket) {
+        try (socket){
+            byte[] buffer = new byte[8192];
+            int n = socket.getInputStream().read(buffer);
+            if(n > 0) {
                 String rawRequest = new String(buffer, 0, n);
                 HttpRequest request = new HttpRequest(rawRequest);
                 request.info(true);
                 dispatcher.execute(request, socket.getOutputStream());
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
     }
 }
